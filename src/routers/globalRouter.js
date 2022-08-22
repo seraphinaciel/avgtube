a7252e464ae663ad71af202bd47260d55b6c2015
seraphinaciel <seraphinaciel@gmail.com>
2022-08-22 20:18:07 +0900

    add routers

---
+++ src/routers/globalRouter.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { join } from "../controllers/userController";
+import { trending } from "../controllers/videoController";
+
+const globalRouter = express.Router();
+
+globalRouter.get("/", trending);
+globalRouter.get("/join", join);
+
+export default globalRouter;
+6  
+src/routers/userRouter.js
