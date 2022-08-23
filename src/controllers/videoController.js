e752f5e78775d8fd7077bd7a5c5cc023232fa980
seraphinaciel <seraphinaciel@gmail.com>
2022-08-23 14:20:50 +0900

    pug templates

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,7 +1,41 @@
-export const trending = (req, res) => res.send("Home Page Videos");
-export const see = (req, res) => res.send("Watch");
-export const watch = (req, res) => res.send("Watch");
-export const edit = (req, res) => res.send("Edit");
+export const trending = (req, res) => {
+  // iteration 사용하려면 배열, 객체로 만들어야 한다.
+  const videos = [
+    {
+      title: "First Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Second Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Third Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+  ];
+  /*
+  render : 000.pug 찾아서 html로 변환해 유저에게 보여줘라.
+  home.pug는 base.pug를 extends. base.pug는 pageTitle를 가지고 있는데 확장한 home.pug에서 변수를 전달해 줘야 함
+  */
+  return res.render("home", { pageTitle: "Home", videos });
+};
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
-export const deleteVideo = (req, res) => res.send("Delete Video");
+export const deleteVideo = (req, res) => {
+  return res.send("Delete Video");
+};
--- src/server.js
+++ src/server.js
@@ -1,35 +1,21 @@
 import express from "express"; // package
-// morgan 패키지는 node.js 서버로 구성된 웹 환경에서 HTTP request 로그를 관리하기 위한 미들웨어이다.lRouter";
 import morgan from "morgan";
-/*
-모든 파일은 독립적인 모듈, 다른 파일에서 사용(공유)하려면 export → import!
-1. default(ex. /routers/globalRouter.js) : 무조건 "하나"만 임포트 하는거라서 마음대로 함수명을 지정 가능
-2. 2개 이상 import, export : 각 함수에 엑스포트 적으면 모두 공유됨. 엑스포트 함수명과 임포트 함수명이 무조건 같게 오브젝트 형식으로 사용
-  export const 함수명1 = (req, res) => res.send("함수명...");
-  import { 함수명1, 함수명2 } from "../000.js";
-  ex. /routers/userRouter.js, /controllers/userController.js
-
-../ 현재 폴더에서 벗어나겠음
-./ 현재 폴더
-*/
 import videoRouter from "./routers/videoRouter"; //export
 import userRouter from "./routers/userRouter";
 
-//4000 port(Door)만 listen(대화)하는 중, 4000 포트로 request 보냄
 const PORT = 4000;
-
 const app = express();
-const logger = morgan("dev");//tiny, common, combined, short
+const logger = morgan("dev");
+
+app.set("view engine", pug); // 퍼그를 뷰 엔진으로 세팅
+// 기본 작업 디렉토리(package.json, /youtube/views) → src 디렉토리로 변경(/youtube/src/views)
+app.set("views", process.cwd() + "/src/views");
 app.use(logger);
-
 app.use("/", globalRouter);
-app.use("/videos", videoRouter); // /routers/videoRouter.js
-app.use("/users", userRouter); // /routers/userRouter.js
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
-// listen시 바로 시작
 const handleListening = () =>
   console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
 
-
-// 서버를 만들면 현재 내용을 지워도 계속 listening한다.
 app.listen(PORT, handleListening)
